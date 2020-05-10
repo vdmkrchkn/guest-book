ff74d2ef5b3f53519aa6a89c2a45c807f8dae68d
vdmkrchkn <vdmkrchkn@gmail.com>
2020-05-10 23:33:31 +0300

    исправлена отправка данных из формы комментария

--- src/js/main.ts
+++ src/js/main.ts
@@ -4,14 +4,66 @@ interface FeedbackComment {
   text: string;
 }
 
-// отправка запроса
-function submitData(data: Event) {
-  data.preventDefault();
+window.onload = function() {
+  const form = document.getElementById('data');
+  if (form) form.onsubmit = submitData;
+  //
+  window.addEventListener('scroll', throttleScroll, false);
+  let isScrolling = false;
+  /** Обёртка для обработчика события прокрутки страницы.
+     * Частота вызова обработчика зависит от частоты обновления кадров страницы.
+     * @param {Event} event - scroll event.
+     */
+  function throttleScroll(event: Event) {
+    if (!isScrolling) {
+      window.requestAnimationFrame(function() {
+        scrollEventHandler(event);
+        isScrolling = true;
+      });
+    }
+
+    isScrolling = false;
+  }
+  const images = document.querySelectorAll('.img-front');
+  /**
+     * Обработчик события прокрутки страницы.
+     * @param {Event} e - scroll event.
+     */
+  function scrollEventHandler(e: Event) {
+    images.forEach(image => {
+      if (isElemVisible(image)) {
+        (image as HTMLElement).style.top = '211px';
+      } else {
+        (image as HTMLElement).style.top = '241px';
+      }
+    });
+  }
+  /**
+     * Определение признака видимости элемента на экране.
+     * @param {Element} element - элемент, для которого определяется видимость.
+     * @param {boolean} isPartially - признак частичной видимости.
+     * @return {boolean} признак видимости.
+     */
+  function isElemVisible(element: Element, isPartially: boolean = false): boolean {
+    const elementBoundary = element.getBoundingClientRect();
+
+    const top = elementBoundary.top;
+    const bottom = elementBoundary.bottom;
+    const height = isPartially ? elementBoundary.height : 0;
+
+    return ((-height <= top) && (bottom <= height + window.innerHeight));
+  }
+};
+
+// eslint-disable-next-line require-jsdoc
+function submitData(event: Event) {
+  event.preventDefault();
+  const formElements = (event.currentTarget as HTMLFormElement).elements;
 
   const comment: FeedbackComment = {
-    name: '', // FIXME: data.currentTarget[0]
-    mail: '',
-    text: '',
+    name: (formElements.namedItem('name') as HTMLInputElement).value,
+    mail: (formElements.namedItem('mail') as HTMLInputElement).value,
+    text: (formElements.namedItem('text') as HTMLTextAreaElement).value,
   };
 
   const xmlHttpObj = window.XMLHttpRequest ? new XMLHttpRequest() : false; // создание объекта XHR
@@ -30,55 +82,3 @@ function submitData(data: Event) {
     xmlHttpObj.send(`name=${comment.name}&mail=${comment.mail}&text=${comment.text}`);
   }
 }
-
-window.onload = function() {
-  // обработчик отправки формы
-  const form = document.getElementById('data');
-  if (form) form.onsubmit = submitData;
-  //
-  window.addEventListener('scroll', throttleScroll, false);
-  let isScrolling = false;
-  /** Обёртка для обработчика события прокрутки страницы.
-   * Частота вызова обработчика зависит от частоты обновления кадров страницы.
-   * @param {Event} event - scroll event.
-   */
-  function throttleScroll(event: Event) {
-    if (!isScrolling) {
-      window.requestAnimationFrame(function() {
-        scrolling(event);
-        isScrolling = true;
-      });
-    }
-
-    isScrolling = false;
-  }
-  const images = document.querySelectorAll('.img-front');
-  /**
-   * Обработчик события прокрутки страницы.
-   * @param {Event} e - scroll event.
-   */
-  function scrolling(e: Event) {
-    images.forEach(image => {
-      if (isElemVisible(image)) {
-        (image as HTMLElement).style.top = '211px';
-      } else {
-        (image as HTMLElement).style.top = '241px';
-      }
-    });
-  }
-  /**
-   * Определение признака видимости элемента на экране.
-   * @param {Element} element - элемент, для которого определяется видимость.
-   * @param {boolean} isPartially - признак частичной видимости.
-   * @return {boolean} признак видимости.
-   */
-  function isElemVisible(element: Element, isPartially: boolean = false): boolean {
-    const elementBoundary = element.getBoundingClientRect();
-
-    const top = elementBoundary.top;
-    const bottom = elementBoundary.bottom;
-    const height = isPartially ? elementBoundary.height : 0;
-
-    return ((-height <= top) && (bottom <= height + window.innerHeight));
-  }
-};
